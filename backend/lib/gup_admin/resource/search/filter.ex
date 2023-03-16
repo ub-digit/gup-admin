76544d625a3444fe2904022cc8fde63920c6c97d
Lars Berglén <lars.berglen@ub.gu.se>
2023-03-16 14:25:50 +0100

    mock up for delivering publications on endpoint publications/duplicates/:id

--- backend/lib/gup_admin/resource/search/filter.ex
+++ backend/lib/gup_admin/resource/search/filter.ex
@@ -6,6 +6,16 @@ defmodule GupAdmin.Resource.Search.Filter do
     |> Enum.map(fn {key, val} -> format_filter_item({key, val}) end)
   end
 
+  def build_duplicate_filter(publication) do
+    %{
+      "scopus_id" => publication["scopus_id"] || nil,
+      "wos_id" => publication["wos_id"] || nil,
+      "doi" => publication["doi"] || nil,
+    }
+    |> Enum.filter(fn {_, val} -> not is_nil(val) end)
+    |> format_filter_item()
+  end
+
   def add_filter(query, nil), do: query
   def add_filter(query, []), do: query
 
@@ -13,6 +23,14 @@ defmodule GupAdmin.Resource.Search.Filter do
     query
     |> put_in(["query", "bool", "filter"], filter)
    # |> IO.inspect(label: "query")
+  end
+
+  def format_filter_item(filter) when is_list(filter) do
+    %{
+      "bool" => %{
+        "should" => List.flatten(Enum.map(filter, fn {key, val} -> %{"match" => %{key => val}} end))
+      }
+    }
   end
 
   def format_filter_item({key, v}) when is_list(v) do
