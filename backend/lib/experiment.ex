f8745ca722eb0edb7bef8bfb104ce17c192757d8
Lars Berglén <lars.berglen@ub.gu.se>
2023-03-21 16:21:39 +0100

    better test data and the possibility to generate custom number of posts. Requires source data from gup server. Route for indexing at /index.

--- backend/lib/experiment.ex
+++ backend/lib/experiment.ex
@@ -1,108 +1,7 @@
 defmodule Experiment do
   @elastic_url "http://localhost:9200"
-  def data do
-    [
-      %{
-        id: 1,
-        authors: ["John Smith"],
-        title: "Lorem Ipsum",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        publication_type: "Doktorsavhandling",
-        publication_date: "2001-05-01",
-        source: "wos"
-      },
-      %{
-        id: 2,
-        authors: ["Jane Doe"],
-        title: "Dolor Sit Amet",
-        description:
-          "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-        publication_type: "Rapport",
-        publication_date: "2002-03-15",
-        source: "scopus"
-      },
-      %{
-        id: 3,
-        authors: ["Bob Johnson"],
-        title: "Consectetur Adipiscing",
-        description:
-          "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        publication_type: "Konstnärligt arbete",
-        publication_date: "2003-09-23",
-        source: "wos"
-      },
-      %{
-        id: 4,
-        authors: ["Alice Williams", "Tom Brown"],
-        title: "Vestibulum Mattis",
-        description:
-          "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        publication_type: "Artikel i vetenskaplig tidskrift",
-        publication_date: "2004-06-30",
-        source: "scopus"
-      },
-      %{
-        id: 5,
-        authors: ["Samantha Lee"],
-        title: "Mauris Auctor",
-        description:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        publication_type: "Patent",
-        publication_date: "2005-11-17",
-        source: "wos"
-      },
-      %{
-        id: 6,
-        authors: ["Mark Jones"],
-        title: "Phasellus Ultrices",
-        description:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        publication_type: "Artikel i vetenskaplig tidskrift",
-        publication_date: "2010-08-01",
-        source: "scopus"
-      },
-      %{
-        id: 7,
-        authors: ["Emily Davis", "David Brown"],
-        title: "Pellentesque Euismod",
-        description:
-          "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-        publication_type: "Doktorsavhandling",
-        publication_date: "2014-07-22",
-        source: "wos"
-      },
-      %{
-        id: 8,
-        authors: ["Alexandra Johnson"],
-        title: "Sed Dignissim",
-        description:
-          "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        publication_type: "Rapport",
-        publication_date: "2017-01-09",
-        source: "scopus"
-      },
-      %{
-        id: 9,
-        authors: ["Chris Lee"],
-        title: "Aenean Mattis",
-        description:
-          "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        publication_type: "Konstnärligt arbete",
-        publication_date: "2019-11-30",
-        source: "wos"
-      },
-      %{
-        id: 10,
-        authors: ["Sarah Taylor"],
-        title: "Fusce Ornare",
-        description:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        publication_type: "Artikel i vetenskaplig tidskrift",
-        publication_date: "2022-02-14",
-        source: "scopus"
-      }
-    ]
-  end
+  @index "publications"
+
 
   def create_index(name) do
     config = %{
@@ -165,295 +64,204 @@ defmodule Experiment do
     end
   end
 
-  def init do
-    create_index("publications")
-    data_set()
+
+
+  # Function that returns a list of filenames from a directory
+  def get_file_names(count \\ 20) do
+    File.ls!("../data/_source")
+    |> Enum.take(count)
+  end
+
+  def init_with_json(count \\ 20) do
+    get_file_names(count)
+    # read all files in list
+    |> Enum.map(fn file -> File.read!("../data/_source/" <> file) end)
+    # parse json
+    |> Enum.map(fn json -> Jason.decode!(json) end)
+    # remap fields
+    |> Enum.map(fn item -> remap_fields(item) end)
+   # |> gupify_data()
+    |> mockup_data(count)
+  end
+
+  def init(publications_count \\ 500) do
+    create_index(@index)
+    init_with_json(publications_count)
     |> Enum.map(fn data ->
       Elastix.Document.index(@elastic_url, "publications", "_doc", data["id"], data, [])
     end)
+
   end
 
-  def data_set do
-    [
+  def gupify_data(data) do
+    data
+    |> Enum.map(fn item ->
+      item
+      |> Map.put("id", "gup:" <> Integer.to_string(item["id"]))
+      |> Map.put("source", "gup")
+    end)
+  end
+
+  def mockup_data(data, count) do
+    split_count = floor(count / 2)
+    {gup, scopus} = Enum.split(data, split_count)
+    scopus = scopus
+    #generate mockup new scopus publications
+    |> Enum.map(fn item ->
+      item
+      |> Map.put("id", "scopus:" <> Integer.to_string(item["id"] + 10000))
+      |> Map.put("source", "scopus")
+      |> Map.put("title", Experiment.NameGen.generate())
+    end)
+
+    # generate duplicate scopus publications
+
+    scopus_duplicates = Enum.take(gup, floor(split_count / 4))
+    |> Enum.map(fn item ->
+      item
+      |> Map.put("id", "scopus:" <> Integer.to_string(item["id"]))
+      |> Map.put("source", "scopus")
+    end)
+
+    # Add source and id to gup publications
+    gup = gup
+    |> Enum.map(fn item ->
+      item
+      |> Map.put("id", "gup:" <> Integer.to_string(item["id"]))
+      |> Map.put("source", "gup")
+    end)
+
+
+    [gup, scopus, scopus_duplicates]
+    |> List.flatten()
+    # |> Enum.map(fn item ->
+    #   %{"title" => item["title"],
+    #   "source" => item["source"],
+    #   "id" => item["id"],
+    # }
+    # |> IO.inspect()
+    #end)
+  end
+
+  def generate_scopus_data(data) do
+    data
+    |> Enum.map(fn item ->
+      item
+      |> Map.put("id", "scopus:" <> Integer.to_string(item["id"] + 10000))
+      |> Map.put("source", "scopus")
+    end)
+  end
+  ########################################################
+  # Functions for remapping fields                      #
+  ########################################################
+
+  def remap_fields(publication) do
+    publication
+    |> Map.put("authors", strip_authors(publication["authors"]))
+  end
+
+
+  def strip_authors([]), do: []
+  def strip_authors(nil), do: []
+  def strip_authors(authors) do
+    authors
+    |> Enum.map(fn author ->
       %{
-        "id" => "1",
-        "title" => "The X Factor",
-        "doi" => "http://doi.org/10.2",
-        "creator" => "1johndoe",
-        "authors" => [
-          %{
-            "id" => "1",
-            "name" => "Alex Smith",
-            "x-account" => "1alexsmith"
-          }
-        ],
-        "gup_id" => "1",
-        "scopus_id" => "1",
-        "date" => "1999",
-        "pubtype" => "11",
-        "number_of_authors" => 1
-      },
-      %{
-        "id" => "2",
-        "title" => "The X-Files",
-        "doi" => "http://doi.org/10.2",
-        "creator" => "2johndoe",
-        "authors" => [
-          %{
-            "id" => "2",
-            "name" => "Brianna Lee",
-            "x-account" => "2briannalee"
-          }
-        ],
-        "gup_id" => "2",
-        "scopus_id" => "2",
-        "date" => "2008",
-        "pubtype" => "10",
-        "number_of_authors" => 1
-      },
-      %{
-        "id" => "3",
-        "title" => "X Marks the Spot",
-        "doi" => "http://doi.org/10.2",
-        "creator" => "3johndoe",
-        "authors" => [
-          %{
-            "id" => "3",
-            "name" => "Caroline Kim",
-            "x-account" => "3carolinekim"
-          }
-        ],
-        "gup_id" => "3",
-        "scopus_id" => "3",
-        "date" => "2015",
-        "pubtype" => "22",
-        "number_of_authors" => 1
-      },
-      %{
-        "id" => "4",
-        "title" => "The X-Men",
-        "doi" => "http://doi.org/10.2",
-        "creator" => "4johndoe",
-        "authors" => [
-          %{
-            "id" => "4",
-            "name" => "David Rodriguez",
-            "x-account" => "4davidrodriguez"
-          }
-        ],
-        "gup_id" => "4",
-        "wos_id" => "4",
-        "date" => "2006",
-        "pubtype" => "18",
-        "number_of_authors" => 1
-      },
-      %{
-        "id" => "5",
-        "title" => "The X-periment: Part I",
-        "doi" => "http://doi.org/10.2",
-        "creator" => "5johndoe",
-        "authors" => [
-          %{
-            "id" => "5",
-            "name" => "Elizabeth Taylor",
-            "x-account" => "5elizabethtaylor"
-          }
-        ],
-        "gup_id" => "5",
-        "wos_id" => "5",
-        "date" => "1988",
-        "pubtype" => "9",
-        "number_of_authors" => 1
-      },
-      %{
-        "id" => "6",
-        "title" => "X-Treme Sports",
-        "doi" => "http://doi.org/10.2",
-        "creator" => "6johndoe",
-        "authors" => [
-          %{
-            "id" => "6",
-            "name" => "Franklin Lee",
-            "x-account" => "6franklinlee"
-          }
-        ],
-        "gup_id" => "6",
-        "scopus_id" => "6",
-        "date" => "1995",
-        "pubtype" => "3",
-        "number_of_authors" => 1
-      },
-      %{
-        "id" => "7",
-        "title" => "X-Ray Vision",
-        "doi" => "http://doi.org/10.2",
-        "creator" => "7johndoe",
-        "authors" => [
-          %{
-            "id" => "7",
-            "name" => "Grace Kim",
-            "x-account" => "7gracekim"
-          }
-        ],
-        "gup_id" => "7",
-        "scopus_id" => "7",
-        "date" => "2003",
-        "pubtype" => "12",
-        "number_of_authors" => 1
-      },
-      %{
-        "id" => "8",
-        "title" => "Xylophone Adventures",
-        "doi" => "http://doi.org/10.2",
-        "creator" => "8johndoe",
-        "authors" => [
-          %{
-            "id" => "8",
-            "name" => "Hannah Nguyen",
-            "x-account" => "8hannahnguyen"
-          }
-        ],
-        "gup_id" => "8",
-        "scopus_id" => "8",
-        "date" => "2017",
-        "pubtype" => "4",
-        "number_of_authors" => 1
-      },
-      %{
-        "id" => "9",
-        "title" => "The X-perience",
-        "doi" => "http://doi.org/10.2",
-        "creator" => "9johndoe",
-        "authors" => [
-          %{
-            "id" => "9",
-            "name" => "Ivy Johnson",
-            "x-account" => "9ivyjohnson"
-          }
-        ],
-        "gup_id" => "9",
-        "wos_id" => "9",
-        "date" => "2019",
-        "pubtype" => "14",
-        "number_of_authors" => 1
-      },
-      %{
-        "id" => "10",
-        "title" => "The X-Factor: A Guide to Success",
-        "doi" => "http://doi.org/10.2",
-        "creator" => "10johndoe",
-        "authors" => [
-          %{
-            "id" => "10",
-            "name" => "Jacob Lee",
-            "x-account" => "10jacoblee"
-          }
-        ],
-        "gup_id" => "10",
-        "wos_id" => "10",
-        "date" => "2022",
-        "pubtype" => "23",
-        "number_of_authors" => 1
-      },
-      %{
-        "id" => "20",
-        "title" => "The Power of Networking",
-        "doi" => "http://doi.org/10.3",
-        "creator" => "20janedoe",
-        "authors" => [
-          %{
-            "id" => "20",
-            "name" => "Emily Chang",
-            "x-account" => "20emilychang"
-          }
-        ],
-        "gup_id" => "20",
-        "date" => "2021",
-        "pubtype" => "18",
-        "number_of_authors" => 1,
-        "source" => "manual"
-      },
-      %{
-        "id" => "21",
-        "title" => "Leadership in the Digital Age",
-        "doi" => "http://doi.org/10.4",
-        "creator" => "21johndoe",
-        "authors" => [
-          %{
-            "id" => "21",
-            "name" => "Olivia Chen",
-            "x-account" => "21oliviachen"
-          }
-        ],
-        "gup_id" => "21",
-        "date" => "2023",
-        "pubtype" => "14",
-        "number_of_authors" => 1,
-        "source" => "manual"
-      },
-      %{
-        "id" => "22",
-        "title" => "Navigating the Gig Economy",
-        "doi" => "http://doi.org/10.5",
-        "creator" => "22johndoe",
-        "authors" => [
-          %{
-            "id" => "22",
-            "name" => "Sophia Rodriguez",
-            "x-account" => "22sophiarodriguez"
-          }
-        ],
-        "gup_id" => "22",
-        "date" => "2020",
-        "pubtype" => "11",
-        "number_of_authors" => 1,
-        "source" => "manual"
-      },
-      %{
-        "id" => "23",
-        "title" => "The Art of Negotiation",
-        "doi" => "http://doi.org/10.6",
-        "creator" => "23johndoe",
-        "authors" => [
-          %{
-            "id" => "23",
-            "name" => "Isabella Kim",
-            "x-account" => "23isabellakim"
-          }
-        ],
-        "gup_id" => "23",
-        "date" => "2019",
-        "pubtype" => "19",
-        "number_of_authors" => 1,
-        "source" => "manual"
-      },
-      %{
-        "id" => "24",
-        "title" => "Innovative Marketing Strategies",
-        "doi" => "http://doi.org/10.7",
-        "creator" => "24johndoe",
-        "authors" => [
-          %{
-            "id" => "24",
-            "name" => "Ethan Lee",
-            "x-account" => "24ethanlee"
-          }
-        ],
-        "gup_id" => "24",
-        "date" => "2022",
-        "pubtype" => "12",
-        "number_of_authors" => 1,
-        "source" => "manual"
+        "id" => author["id"],
+        "name" => get_full_name(author["first_name"], author["last_name"]),
+        "x-account" => get_x_account(author["identifiers"]),
+        "departments" => get_departments(author["departments"])
       }
-    ]
-    |> Enum.map(fn item -> set_source(item) end)
+    end)
   end
 
-  def set_source(%{"source" => _} = item), do: item
-  def set_source(item) do
-    case Map.has_key?(item, "wos_id") do
-      true -> Map.put(item, "source", "wos")
-      false -> Map.put(item, "source", "scopus")
+  def get_full_name(nil, nil), do: ""
+  def get_full_name(nil, last_name), do: last_name
+  def get_full_name(first_name, nil), do: first_name
+  def get_full_name(first_name, last_name), do: first_name <> " " <> last_name
+
+
+
+  def get_x_account([]), do: nil
+
+  def get_x_account(identifiers) do
+    identifiers
+    |> Enum.find(fn identifier -> identifier["source_name"] == "xkonto" end)
+    |> case do
+      nil -> ""
+      map -> Map.get(map, "value")
     end
   end
+
+  def get_departments([]), do: nil
+  def get_departments(department) do
+   %{
+      "id" => department["id"],
+      "name" => department["name"],
+      "faculty" => get_faculty(department["faculty"]),
+   }
+  end
+
+  def get_faculty(faculty) do
+    %{
+      "id" => faculty["id"],
+      "name" => faculty["name"]
+    }
+  end
+  def benchmark(name, fun, args) do
+    start = System.monotonic_time(:millisecond)
+    fun.(args)
+    finish = System.monotonic_time(:millisecond)
+    IO.puts("#{name} took #{finish - start} ms")
+  end
+
+  def get_publication_type_list(publications) do
+    publications
+    |> Enum.map(fn publication ->
+      %{
+        "publications_type_id" => publication["publication_type_id"],
+        "publications_type_label" => publication["publication_type_label"]
+      }
+      end)
+    |> Enum.uniq()
+    |> Enum.sort_by(fn pub_type -> pub_type["publications_type_id"] end)
+  end
+
+
+  def db_txt() do
+    "  1 | conference_other
+    2 | conference_paper
+    3 | conference_poster
+    5 | publication_journal-article
+    7 | publication_magazine-article
+    8 | publication_edited-book
+    9 | publication_book
+   10 | publication_book-chapter
+   13 | intellectual-property_patent
+   16 | publication_report
+   17 | publication_doctoral-thesis
+   18 | publication_book-review
+   19 | publication_licentiate-thesis
+   21 | other
+   22 | publication_review-article
+   23 | artistic-work_scientific_and_development
+   28 | publication_textcritical-edition
+   30 | publication_textbook
+   34 | artistic-work_original-creative-work
+   40 | publication_editorial-letter
+   41 | publication_report-chapter
+   42 | publication_newspaper-article
+   43 | publication_encyclopedia-entry
+   44 | publication_journal-issue
+   45 | conference_proceeding
+   46 | publication_working-paper"
+  |> String.split("\n")
+  |> Enum.map(fn line ->
+    %{
+      "publications_type_id" => line |> String.split("|") |> List.first() |> String.trim() |> String.to_integer(),
+      "publications_type_label" => line |> String.split("|") |> List.last() |> String.trim()
+    }
+    end)
+  end
+
 end
--- backend/lib/gup_admin/model/publication_type.ex
+++ backend/lib/gup_admin/model/publication_type.ex
@@ -3,31 +3,107 @@
 defmodule GupAdmin.Model.PublicationType do
   def publication_types do
     [
-      %{"id" => 1, "display_name" => "Textkritisk utgåva"},
-      %{"id" => 2, "display_name" => "Annan publikation"},
-      %{"id" => 3, "display_name" => "Doktorsavhandling"},
-      %{"id" => 4, "display_name" => "Rapport"},
-      %{"id" => 5, "display_name" => "Paper i proceeding"},
-      %{"id" => 6, "display_name" => "Konferensbidrag (offentliggjort, men ej förlagsutgivet)"},
-      %{"id" => 7, "display_name" => "Samlingsverk (red.)"},
-      %{"id" => 8, "display_name" => "Kapitel i rapport"},
-      %{"id" => 9, "display_name" => "Artikel i dagstidning"},
-      %{"id" => 10, "display_name" => "Bidrag till encyklopedi"},
-      %{"id" => 11, "display_name" => "Licentiatsavhandling"},
-      %{"id" => 12, "display_name" => "Special / temanummer av tidskrift (red.)"},
-      %{"id" => 13, "display_name" => "Poster (konferens)"},
-      %{"id" => 14, "display_name" => "Proceeding (red.)"},
-      %{"id" => 15, "display_name" => "Patent"},
-      %{"id" => 16, "display_name" => "Konstnärligt forsknings- och utvecklingsarbete"},
-      %{"id" => 17, "display_name" => "Kapitel i bok"},
-      %{"id" => 18, "display_name" => "Forskningsöversiktsartikel (Review article)"},
-      %{"id" => 19, "display_name" => "Artikel i vetenskaplig tidskrift"},
-      %{"id" => 20, "display_name" => "Inledande text i tidskrift"},
-      %{"id" => 21, "display_name" => "Recension"},
-      %{"id" => 22, "display_name" => "Artikel i övriga tidskrifter"},
-      %{"id" => 23, "display_name" => "Bok"},
-      %{"id" => 24, "display_name" => "Working paper"},
-      %{"id" => 25, "display_name" => "Konstnärligt arbete"}
+      %{
+        "publications_type_id" => 1,
+        "publications_type_label" => "conference_other"
+      },
+      %{
+        "publications_type_id" => 2,
+        "publications_type_label" => "conference_paper"
+      },
+      %{
+        "publications_type_id" => 3,
+        "publications_type_label" => "conference_poster"
+      },
+      %{
+        "publications_type_id" => 5,
+        "publications_type_label" => "publication_journal-article"
+      },
+      %{
+        "publications_type_id" => 7,
+        "publications_type_label" => "publication_magazine-article"
+      },
+      %{
+        "publications_type_id" => 8,
+        "publications_type_label" => "publication_edited-book"
+      },
+      %{
+        "publications_type_id" => 9,
+        "publications_type_label" => "publication_book"
+      },
+      %{
+        "publications_type_id" => 10,
+        "publications_type_label" => "publication_book-chapter"
+      },
+      %{
+        "publications_type_id" => 13,
+        "publications_type_label" => "intellectual-property_patent"
+      },
+      %{
+        "publications_type_id" => 16,
+        "publications_type_label" => "publication_report"
+      },
+      %{
+        "publications_type_id" => 17,
+        "publications_type_label" => "publication_doctoral-thesis"
+      },
+      %{
+        "publications_type_id" => 18,
+        "publications_type_label" => "publication_book-review"
+      },
+      %{
+        "publications_type_id" => 19,
+        "publications_type_label" => "publication_licentiate-thesis"
+      },
+      %{"publications_type_id" => 21, "publications_type_label" => "other"},
+      %{
+        "publications_type_id" => 22,
+        "publications_type_label" => "publication_review-article"
+      },
+      %{
+        "publications_type_id" => 23,
+        "publications_type_label" => "artistic-work_scientific_and_development"
+      },
+      %{
+        "publications_type_id" => 28,
+        "publications_type_label" => "publication_textcritical-edition"
+      },
+      %{
+        "publications_type_id" => 30,
+        "publications_type_label" => "publication_textbook"
+      },
+      %{
+        "publications_type_id" => 34,
+        "publications_type_label" => "artistic-work_original-creative-work"
+      },
+      %{
+        "publications_type_id" => 40,
+        "publications_type_label" => "publication_editorial-letter"
+      },
+      %{
+        "publications_type_id" => 41,
+        "publications_type_label" => "publication_report-chapter"
+      },
+      %{
+        "publications_type_id" => 42,
+        "publications_type_label" => "publication_newspaper-article"
+      },
+      %{
+        "publications_type_id" => 43,
+        "publications_type_label" => "publication_encyclopedia-entry"
+      },
+      %{
+        "publications_type_id" => 44,
+        "publications_type_label" => "publication_journal-issue"
+      },
+      %{
+        "publications_type_id" => 45,
+        "publications_type_label" => "conference_proceeding"
+      },
+      %{
+        "publications_type_id" => 46,
+        "publications_type_label" => "publication_working-paper"
+      }
     ]
   end
 end
