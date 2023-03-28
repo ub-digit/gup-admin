e30d3caadab8c54f633c6b47ff01f91696a4d9af
Johan Larsson <karmajohan@gmail.com>
2023-03-28 14:15:34 +0200

    changed to use code instead of statuscode

--- frontend-nuxt/components/ErrorLoadingPost.vue
+++ frontend-nuxt/components/ErrorLoadingPost.vue
@@ -1,7 +1,7 @@
 <template>
     <div v-if="error" class="row">
         <div class="col text-center">
-            <h2 class="mb-2 text-danger"><font-awesome-icon  icon="fa-solid fa-circle-exclamation" /> {{ error.statusCode }}</h2>
+            <h2 class="mb-2 text-danger"><font-awesome-icon  icon="fa-solid fa-circle-exclamation" /> {{ error.code }}</h2>
             <p> 
                 {{ error.message }}
             </p>
