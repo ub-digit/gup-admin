1f2602dda261a8d2a46fe6d466152be4a0d228ff
Johan Larsson <karmajohan@gmail.com>
2023-04-04 16:02:54 +0200

    support linking title

--- frontend-nuxt/components/PostDisplayTitle.vue
+++ frontend-nuxt/components/PostDisplayTitle.vue
@@ -1,14 +1,21 @@
 <template>
     <div>
         <div class="post-title p-2 mb-4" :class="{'do-not-truncate' : isExpanded }">
-            <h2 class="pb-2 mb-0"><span v-if="!isExpanded">{{ post_title_truncated }}</span><span v-else>{{ props.title }}</span></h2> 
+            <a v-if="props.url" :href="props.url" target="_blank" class="no-color">
+                <h2 class="pb-2 mb-0"><span v-if="!isExpanded">{{ post_title_truncated }}</span><span v-else>{{ props.title }}</span></h2> 
+            </a>
+            <span v-else>
+                <h2 class="pb-2 mb-0"><span v-if="!isExpanded">{{ post_title_truncated }}</span><span v-else>{{ props.title }}</span></h2> 
+            </span>
+                
+
             <a href="javascript:void(0)" v-if="showToggle" @click.prevent="isExpanded = !isExpanded"><span v-if="isExpanded">- Visa mindre</span><span v-else>+ Visa mer</span></a>
         </div>
     </div>
 </template>
 
 <script setup>
-const props = defineProps(['title'])
+const props = defineProps(['title', 'url'])
 const maxLength = 85;
 let isExpanded = ref(false);
 const showToggle = computed(()=> {
@@ -28,6 +35,9 @@ const post_title_truncated = computed(() => {
 
 <style lang="scss" scoped>
 .post-title {
+    .no-color {
+        color: rgb(51, 51,51);
+    }
     min-height: 11em;
     &.do-not-truncate {
         height: auto;
