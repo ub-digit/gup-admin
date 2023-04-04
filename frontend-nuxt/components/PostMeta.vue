ce098ea38b8688a5a65b12966ec6dd5108b3c7c7
Johan Larsson <karmajohan@gmail.com>
2023-04-04 16:05:35 +0200

    add date and show only info when availible

--- frontend-nuxt/components/PostMeta.vue
+++ frontend-nuxt/components/PostMeta.vue
@@ -1,9 +1,9 @@
 <template>
     <div>
         <ul class="list-inline">
-          <li v-if="props.post.needs_attention" class="list-inline-item"><font-awesome-icon class="text-danger" icon="fa-solid fa-flag" /> {{ t('views.publications.form.needs_attention') }}</li>
-          <li class="list-inline-item"><font-awesome-icon class="text-info" icon="fa-solid fa-file-arrow-down" /> {{ t('views.publications.post.import_source') }} {{ props.post.source }}</li>
-          <li v-if="props.post.version_created_by" class="list-inline-item"><font-awesome-icon class="text-warning" icon="fa-solid fa-award" /> {{ props.post.date }} {{ t("views.publications.post.created_by") }} {{ props.post.version_created_by }}</li>
+          <li v-if="!props.post.attended" class="list-inline-item"><font-awesome-icon class="text-danger" icon="fa-solid fa-flag" /> {{ t('views.publications.form.needs_attention') }}</li>
+          <li class="list-inline-item"><font-awesome-icon class="text-info d-none" icon="fa-solid fa-file-arrow-down" /> <strong>{{ t('views.publications.post.import_source') }} </strong> {{ props.post.source }}</li>
+          <li v-if="props.post.version_updated_by" class="list-inline-item"><font-awesome-icon class="text-warning d-none" icon="fa-solid fa-award" /> <strong>{{ t("views.publications.post.updated") }}</strong> {{ getDate }} {{t("views.publications.post.updated_by")}} {{ props.post.version_updated_by }}</li>
         </ul>
     </div>
 </template>
@@ -11,6 +11,11 @@
 <script setup>
 const {t} = useI18n();
 const props = defineProps(['post'])
+
+const getDate = computed(() => {
+  let date = new Date(props.post.updated_at);
+  return date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate()
+})
 
 </script>
 
