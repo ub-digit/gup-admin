d368404d4b66d5faf6775b8c9f3c8087fa7f922d
Johan Larsson <karmajohan@gmail.com>
2023-04-12 13:45:47 +0200

    rowbreaks added

--- frontend-nuxt/components/PostMeta.vue
+++ frontend-nuxt/components/PostMeta.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <ul class="list-inline">
-          <li v-if="!props.post.attended" class="list-inline-item"><font-awesome-icon class="text-danger" icon="fa-solid fa-flag" /> {{ t('views.publications.form.needs_attention') }}</li>
+          <li v-if="!props.post.attended" class="list-inline-item"><font-awesome-icon class="text-danger" icon="fa-solid fa-flag" /> {{ t('views.publications.form.needs_attention') }}</li><br><br>
           <li class="list-inline-item"><font-awesome-icon class="text-info d-none" icon="fa-solid fa-file-arrow-down" /> <strong>{{ t('views.publications.post.import_source') }} </strong> {{ props.post.source }}</li>
           <li v-if="props.post.created_at" class="list-inline-item"><font-awesome-icon class="text-warning d-none" icon="fa-solid fa-award" /> <strong>{{ t("views.publications.post.created") }}</strong> {{ getDate(props.post.created_at) }} {{t("views.publications.post.created_by")}} {{ props.post.version_created_by }}</li>
           <li v-if="props.post.version_updated_by" class="list-inline-item"><font-awesome-icon class="text-warning d-none" icon="fa-solid fa-award" /> <strong>{{ t("views.publications.post.updated") }}</strong> {{ getDate(props.post.updated_at) }} {{t("views.publications.post.updated_by")}} {{ props.post.version_updated_by }}</li>
