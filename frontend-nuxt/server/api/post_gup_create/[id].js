98805a3d86379d21c987bf30ff087bc8753dee2d
Johan Larsson <karmajohan@gmail.com>
2023-04-13 11:33:26 +0200

    new endpoint for gup create

---
+++ frontend-nuxt/server/api/post_gup_create/[id].js
@@ -0,0 +1,7 @@
+export default defineEventHandler(async (event) => {
+    const config = useRuntimeConfig();
+    const query = getQuery(event);
+    const id = event.context.params.id;
+    const res = await $fetch(`${config.API_BASE_URL}/publications/post_to_gup/${id}`, {method: 'POST'});
+    return res;
+})
