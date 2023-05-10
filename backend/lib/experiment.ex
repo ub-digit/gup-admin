95668b689645dbc06c32a53a2f99998375e6a7ad
Lars Berglén <lars.berglen@ub.gu.se>
2023-05-10 11:04:26 +0200

    Now populate index with both gup and scopus data

--- backend/lib/experiment.ex
+++ backend/lib/experiment.ex
@@ -1,6 +1,5 @@
 defmodule Experiment do
   @index "publications"
-  @index_url "http://localhost:9200"
 
   def elastic_url do
     System.get_env("ELASTIC_SEARCH_URL") || "http://localhost:9200"
@@ -8,59 +7,7 @@ defmodule Experiment do
 
 
   def create_index(name) do
-    # config = %{
-    #   "mappings" => %{
-    #     "properties" => %{
-    #       "title" => %{
-    #         "fields" => %{
-    #           "sort" => %{
-    #             "type" => "icu_collation_keyword",
-    #             "language" => "sv",
-    #             "country" => "SE"
-    #           }
-    #         },
-    #         "type" => "text",
-    #         "analyzer" => "edge_ngram_analyzer",
-    #         "search_analyzer" => "standard"
-    #       }
-    #     }
-    #   },
-    #   "settings" => %{
-    #     "analysis" => %{
-    #       "analyzer" => %{
-    #         "edge_ngram_analyzer" => %{
-    #           "tokenizer" => "edge_ngram_tokenizer",
-    #           "filter" => [
-    #             "lowercase"
-    #           ]
-    #         }
-    #       },
-    #       "tokenizer" => %{
-    #         "edge_ngram_tokenizer" => %{
-    #           "type" => "edge_ngram",
-    #           "min_gram" => 2,
-    #           "max_gram" => 20,
-    #           "token_chars" => [
-    #             "letter",
-    #             "digit",
-    #             "custom"
-    #           ], "custom_token_chars" => [
-    #             "å",
-    #             "ä",
-    #             "ö",
-    #             "Å",
-    #             "Ä",
-    #             "Ö",
-    #             "-"
-    #           ]
-    #         }
-    #       }
-    #     }
-    #   }
-    # }
-
     Elastix.Index.delete(elastic_url(), name)
-
     Elastix.Index.create(elastic_url(), name, config())
     |> case do
       {:ok, %{body: %{"error" => reason}}} -> {:error, reason}
@@ -71,26 +18,35 @@ defmodule Experiment do
 
 
   # Function that returns a list of filenames from a directory
-  def get_file_names(count \\ 20) do
+  def get_gup_file_names() do
     File.ls!("../data/_source")
-    |> Enum.take(count)
+    |> Enum.reject(fn file -> file == ".gitignore" end)
   end
 
-  def init_with_json(count \\ 20) do
-    get_file_names(count)
+  def load_gup_data(count \\ 1_000_000) do
+    get_gup_file_names()
+    |> Enum.take(count)
     # read all files in list
     |> Enum.map(fn file -> File.read!("../data/_source/" <> file) end)
     # parse json
     |> Enum.map(fn json -> Jason.decode!(json) end)
     # remap fields
     |> Enum.map(fn item -> remap_fields(item) end)
-   # |> gupify_data()
-    |> mockup_data(count)
+    |> Enum.map(fn item -> Map.put(item, "source", "gup") end)
+    |> Enum.map(fn item -> Map.put(item, "id", "gup_" <> Integer.to_string(item["id"])) end)
+    |> Enum.map(fn item -> Map.put(item, "attended", "false") end)
   end
 
-  def init(publications_count \\ 500) do
+  def append_list(a, b) do
+    a ++ b
+  end
+
+
+  def init(count \\ 1_000_000) do
     create_index(@index)
-    init_with_json(publications_count)
+    load_gup_data(count)
+    |> append_list(load_scopus_data(count))
+    |> Enum.map(fn post -> Map.put(post, "deleted", false) end)
     #|> Enum.map(fn data -> %{"title" => data["title"], "id" => data["id"], "attended" => data["attended"], "deleted" => data["deleted"], "source" => data["source"], "pubyear" => data["pubyear"]} end)
     #|> Enum.map(fn data -> %{"title" => data["title"], "id" => data["id"]} end)
     |> Enum.map(fn data ->
@@ -99,69 +55,7 @@ defmodule Experiment do
 
   end
 
-  def gupify_data(data) do
-    data
-    |> Enum.map(fn item ->
-      item
-      |> Map.put("id", "gup:" <> Integer.to_string(item["id"]))
-      |> Map.put("source", "gup")
-    end)
-  end
 
-  def mockup_data(data, count) do
-    split_count = floor(count / 2)
-    {gup, scopus} = Enum.split(data, split_count)
-    scopus = scopus
-    #generate mockup new scopus publications
-    |> Enum.map(fn item ->
-      item
-      |> Map.put("id", "scopus_" <> Integer.to_string(item["id"] + 10000))
-      |> Map.put("source", "scopus")
-      |> Map.put("title", Experiment.NameGen.generate())
-      |> Map.put("attended", true)
-    end)
-
-    # generate duplicate scopus publications
-
-    scopus_duplicates = Enum.take(gup, floor(split_count / 4))
-    |> Enum.map(fn item ->
-      item
-      |> Map.put("id", "scopus_" <> Integer.to_string(item["id"]))
-      |> Map.put("source", "scopus")
-      |> Map.put("attended", true)
-    end)
-
-    # Add source and id to gup publications and needs_attention
-    gup = gup
-    |> Enum.map(fn item ->
-      item
-      |> Map.put("id", "gup_" <> Integer.to_string(item["id"]))
-      |> Map.put("source", "gup")
-      |> Map.put("attended", true)
-    end)
-    |> Enum.map_every(2, fn item -> Map.put(item, "attended", false) end)
-
-
-    [gup, scopus, scopus_duplicates]
-    |> List.flatten()
-    |> Enum.map(fn p -> Map.put(p, "deleted", false) end)
-    # |> Enum.map(fn item ->
-    #   %{"title" => item["title"],
-    #   "source" => item["source"],
-    #   "id" => item["id"],
-    # }
-    # |> IO.inspect()
-    #end)
-  end
-
-  def generate_scopus_data(data) do
-    data
-    |> Enum.map(fn item ->
-      item
-      |> Map.put("id", "scopus_" <> Integer.to_string(item["id"] + 10000))
-      |> Map.put("source", "scopus")
-    end)
-  end
   ########################################################
   # Functions for remapping fields                      #
   ########################################################
@@ -239,55 +133,9 @@ defmodule Experiment do
   end
 
 
-  def db_txt() do
-    "30 | publication_textbook                     | Lärobok
-    28 | publication_textcritical-edition         | Textkritisk utgåva
-    21 | other                                    | Annan publikation
-    17 | publication_doctoral-thesis              | Doktorsavhandling
-    16 | publication_report                       | Rapport
-     2 | conference_paper                         | Paper i proceeding
-     1 | conference_other                         | Konferensbidrag (offentliggjort, men ej förlagsutgivet)
-     8 | publication_edited-book                  | Samlingsverk (red.)
-    41 | publication_report-chapter               | Kapitel i rapport
-    42 | publication_newspaper-article            | Artikel i dagstidning
-    43 | publication_encyclopedia-entry           | Bidrag till encyklopedi
-    19 | publication_licentiate-thesis            | Licentiatsavhandling
-    44 | publication_journal-issue                | Special / temanummer av tidskrift (red.)
-     3 | conference_poster                        | Poster (konferens)
-    45 | conference_proceeding                    | Proceeding (red.)
-    13 | intellectual-property_patent             | Patent
-    23 | artistic-work_scientific_and_development | Konstnärligt forsknings- och utvecklingsarbete
-    10 | publication_book-chapter                 | Kapitel i bok
-    22 | publication_review-article               | Forskningsöversiktsartikel (Review article)
-     5 | publication_journal-article              | Artikel i vetenskaplig tidskrift
-    40 | publication_editorial-letter             | Inledande text i tidskrift
-    18 | publication_book-review                  | Recension
-     7 | publication_magazine-article             | Artikel i övriga tidskrifter
-     9 | publication_book                         | Bok
-    46 | publication_working-paper                | Working paper
-    34 | artistic-work_original-creative-work     | Konstnärligt arbete"
-  |> String.split("\n")
-  |> Enum.map(fn line ->
-     line = line |> String.split("|") |> List.to_tuple()
 
-    %{
-      "publication_type_id" => elem(line, 0) |> String.trim() |> String.to_integer(),
-      "publication_type_code" => elem(line, 1) |> String.trim(),
-      "publication_type_label" => elem(line, 2) |> String.trim()
-    }
-    end)
-      |> Enum.sort_by(fn pub_type -> pub_type["publication_type_label"] end)
-    end
 
-    def i_test do
-      Elastix.Index.delete(@index_url, "search_test")
-      Elastix.Index.create(@index_url, "search_test", config())
-      mock_data()
-      |> IO.inspect(label: "mock_data")
-      |> Enum.map(fn item ->
-        Elastix.Document.index(@index_url, "search_test", "_doc", item["id"], item, [])
-      end)
-    end
+
 
     def config do
 
@@ -349,20 +197,37 @@ defmodule Experiment do
 
     end
 
-    def mock_data do
-      1..30
-      |> Enum.map(fn i -> %{ "id" => i, "title" => Experiment.NameGen.generate()} end)
+
+
+
+    def load_scopus_data(count \\ 1_000_000) do
+      get_scopus_file_list()
+      |> Enum.take(count)
+      |> Enum.map(fn file ->
+        File.read!(file)
+        |> Jason.decode!()
+      end)
+      |> Enum.map(&set_scopus_id/1)
+      |> Enum.map(fn item -> Map.put(item, "source", "scopus")end)
+      |> Enum.map(fn item -> Map.put(item, "attended", "false") end)
     end
 
-    def a | b do
-      merge_rules = fn key, a_val, b_val ->
-        case key do
-          "id" -> a_val
-          _ -> b_val
-        end
-
-      end
-      Map.merge(a, b, merge_rules)
+    def set_scopus_id(post) do
+      id = Map.get(post, "publication_identifiers")
+      |> Enum.find(fn identifier -> identifier["identifier_code"] == "scopus-id" end)
+      |> Map.get("identifier_value")
+      Map.put(post, "id", "scopus_" <> id)
     end
 
+    def get_scopus_file_list do
+      File.ls!("../data/scopus-normalised/")
+      |> Enum.reject(fn dir -> dir == ".gitignore" end)
+      |> List.flatten()
+      |> Enum.map(fn dir -> Path.join("../data/scopus-normalised/", dir) end)
+      |> Enum.map(fn dir ->
+        File.ls!(dir)
+        |> Enum.map(fn file -> Path.join(dir, file) end)
+      end)
+      |> List.flatten()
+    end
   end
