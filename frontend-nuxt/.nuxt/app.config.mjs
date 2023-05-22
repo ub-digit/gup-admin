884a85d18cfed1580d9c706ac0ab61736fd1ab9d
Johan Larsson <karmajohan@gmail.com>
2023-05-22 15:47:03 +0200

    removed generated

--- frontend-nuxt/.nuxt/app.config.mjs
+++
@@ -1,8 +0,0 @@
-
-import { defuFn } from '/home/lars/Documents/workspaces/gup-admin/frontend-nuxt/node_modules/defu/dist/defu.mjs'
-
-const inlineConfig = {}
-
-
-
-export default /* #__PURE__ */ defuFn(inlineConfig)
