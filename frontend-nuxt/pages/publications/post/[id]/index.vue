1a3c70db27899a8259e772c02d5cce1ab03f2a38
Johan Larsson <karmajohan@gmail.com>
2023-09-05 15:14:48 +0200

    autoformat with prettier

--- frontend-nuxt/pages/publications/post/[id]/index.vue
+++ frontend-nuxt/pages/publications/post/[id]/index.vue
@@ -1,11 +1,11 @@
 <template>
-    <NothingSelected/>
+  <NothingSelected />
 </template>
 
 <script setup>
-    import { useGupPostsStore } from '~/store/gup_posts'
-    import { storeToRefs } from 'pinia'
-    const gupPostsStore = useGupPostsStore()
-    const {$reset} = gupPostsStore
-    $reset();
+import { useGupPostsStore } from "~/store/gup_posts";
+import { storeToRefs } from "pinia";
+const gupPostsStore = useGupPostsStore();
+const { $reset } = gupPostsStore;
+$reset();
 </script>
