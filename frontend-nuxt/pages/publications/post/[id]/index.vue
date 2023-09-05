f206de2eef55792ebc21c34bfc94510cac968689
Johan Larsson <karmajohan@gmail.com>
2023-09-05 15:16:28 +0200

    removed unused imports

--- frontend-nuxt/pages/publications/post/[id]/index.vue
+++ frontend-nuxt/pages/publications/post/[id]/index.vue
@@ -2,10 +2,4 @@
   <NothingSelected />
 </template>
 
-<script setup>
-import { useGupPostsStore } from "~/store/gup_posts";
-import { storeToRefs } from "pinia";
-const gupPostsStore = useGupPostsStore();
-const { $reset } = gupPostsStore;
-$reset();
-</script>
+<script setup></script>
