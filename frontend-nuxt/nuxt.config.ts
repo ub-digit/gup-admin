aa33f9a6c253140ed78aa52b837d486316c72c12
Johan Larsson <karmajohan@gmail.com>
2023-09-06 10:43:40 +0200

    fix urls in config

--- frontend-nuxt/nuxt.config.ts
+++ frontend-nuxt/nuxt.config.ts
@@ -1,39 +1,35 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    ssr: true,
-    runtimeConfig: {
-        API_BASE_URL: 'http://localhost:4000',
-        public: {
-          API_GUP_BASE_URL_EDIT: 'https://gup-lab.ub.gu.se/publications/show/',
-          API_GUP_BASE_URL_SHOW: 'https://gup-lab.ub.gu.se/publications/show/',
-        
-        }
-/*         server: {
+  ssr: false,
+  runtimeConfig: {
+    API_BASE_URL: "http://localhost:40411/",
+    public: {
+      API_GUP_BASE_URL_EDIT: "http://localhost:3010/publications/show/",
+      API_GUP_BASE_URL_SHOW: "http://localhost:3010/publications/show/",
+    },
+    /*         server: {
           host: process.env.NUXT_HOST,
           port: process.env.NUXT_PORT,
         },   */
-    },
+  },
 
-    typescript: {
-        shim: false
-    },
-    modules: [
-        '@pinia/nuxt',
-        '@vueuse/nuxt'
+  typescript: {
+    shim: false,
+  },
+  modules: ["@pinia/nuxt", "@vueuse/nuxt"],
+  build: {
+    transpile: [
+      "@fortawesome/fontawesome-svg-core",
+      "@fortawesome/free-brands-svg-icons",
+      "@fortawesome/free-regular-svg-icons",
+      "@fortawesome/free-solid-svg-icons",
+      "@fortawesome/vue-fontawesome",
     ],
-    build: {
-        transpile: [
-          '@fortawesome/fontawesome-svg-core',
-          '@fortawesome/free-brands-svg-icons',
-          '@fortawesome/free-regular-svg-icons',
-          '@fortawesome/free-solid-svg-icons',
-          '@fortawesome/vue-fontawesome'
-        ]
-      },
-    css: [
-        // SCSS file in the project
-        '@/assets/main.scss',
-        '@fortawesome/fontawesome-svg-core/styles.css',
-        'vue-toast-notification/dist/theme-default.css'
-      ],
-})
+  },
+  css: [
+    // SCSS file in the project
+    "@/assets/main.scss",
+    "@fortawesome/fontawesome-svg-core/styles.css",
+    "vue-toast-notification/dist/theme-default.css",
+  ],
+});
