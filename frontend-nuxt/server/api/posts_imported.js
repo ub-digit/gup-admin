07c803b945ebce214fa3e4f35636e15c7cea15f4
Johan Larsson <karmajohan@gmail.com>
2023-09-15 14:26:04 +0200

    removes console.log

--- frontend-nuxt/server/api/posts_imported.js
+++ frontend-nuxt/server/api/posts_imported.js
@@ -1,7 +1,6 @@
 export default defineEventHandler(async (event) => {
   const config = useRuntimeConfig();
   const query = getQuery(event);
-  console.log(query);
   const res = await $fetch(`${config.API_BASE_URL}/publications`, {
     params: query,
   });
