ebfd7ff28070ff823c32a4eaa9e02429e5dda058
Johan Larsson <karmajohan@gmail.com>
2023-09-26 10:28:02 +0200

    removed commented code

--- frontend-nuxt/store/gup_posts.js
+++ frontend-nuxt/store/gup_posts.js
@@ -21,12 +21,6 @@ export const useGupPostsStore = defineStore("gupPostsStore", () => {
       if (data.value.error) {
         throw data.value;
       }
-      /*       if (data.value.error) {
-        // Convert to format used in catch to be able to use same
-        var temp = ref(null);
-        temp.value = { data: { data: data.value } };
-        throw temp;
-      } */
       gupCompareImportedMatrix.value = data.value;
     } catch (error) {
       errorGupCompareImportedMatrix.value = error;
