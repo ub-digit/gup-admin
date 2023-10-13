0a614401fa14b3acccc5f4a13934d0b43cc6df30
Lars Berglén <lars.berglen@ub.gu.se>
2023-10-13 10:48:04 +0200

    removed api keys from controller

--- gup_index_manager/lib/gup_index_manager_web/controllers/publication_controller.ex
+++ gup_index_manager/lib/gup_index_manager_web/controllers/publication_controller.ex
@@ -2,32 +2,41 @@
 defmodule GupIndexManagerWeb.PublicationController do
   use GupIndexManagerWeb, :controller
   alias GupIndexManager.Resource.Publication
-  def create_or_update(conn,  %{"data" => data, "api_key" => "megasecretimpossibletoguesskey"}) do
-    IO.inspect(data, label: "GupIndexManagerWeb.PublicationController.create_or_update")
-    Publication.create_or_update(data)
-    json conn, %{status: "ok"}
+  def create_or_update(conn,  %{"data" => data, "api_key" => api_key}) do
+    case api_key == get_api_key() do
+      true ->
+        Publication.create_or_update(data)
+        json conn, %{status: "ok"}
+      false ->
+        json conn, %{status: "error, unauthorized key"}
+    end
   end
 
-  def create_or_update(conn, _params) do
-    json conn, %{status: "error"}
+  def delete(conn,  %{"id" => id, "api_key" => api_key}) do
+    case api_key == get_api_key() do
+      true ->
+        Publication.delete(id)
+        json conn, %{status: "ok"}
+      false ->
+        json conn, %{status: "error, unauthorized key"}
+    end
   end
 
-  def delete(conn,  %{"id" => id, "api_key" => "megasecretimpossibletoguesskey"}) do
-    IO.inspect(id, label: "GupIndexManagerWeb.PublicationController.delete")
-    Publication.delete(id)
-    json conn, %{status: "ok"}
-  end
-
-  def delete(conn, _params) do
-    json conn, %{status: "error"}
-  end
-
-  def list(conn, _params) do
+  def list(conn, %{"api_key" => api_key}) do
     json conn, Publication.list()
   end
 
-  def mark_as_pending(conn, %{"id" => id, "api_key" => "megasecretimpossibletoguesskey"}) do
-    Publication.mark_as_pending(id)
-    json conn, %{status: "ok"}
+  def mark_as_pending(conn, %{"id" => id, "api_key" => api_key) do
+    case api_key == get_api_key() do
+      true ->
+        Publication.mark_as_pending(id)
+        json conn, %{status: "ok"}
+      false ->
+        json conn, %{status: "error, unauthorized key"}
+    end
+  end
+
+  def get_api_key() do
+    System.get_env("GUP_INDEX_MANAGER_API_KEY")
   end
 end
