a4e3edb8e9eb50d91c0c232f514dc2b1fdca2240
Johan Larsson <johan.larsson@ub.gu.se>
2024-02-20 13:15:38 +0100

    start typescript conversion

--- frontend-nuxt/store/publication_types.ts
+++ frontend-nuxt/store/publication_types.ts
@@ -1,12 +1,13 @@
 import { defineStore } from "pinia";
+import type { PublicationType } from "~/types/PublicationType";
 
 export const usePublicationTypesStore = defineStore(
   "publicationTypesStore",
   () => {
-    const publicationTypes = ref([]);
+    const publicationTypes: Ref<PublicationType[]> = ref([]);
     const pendingPublicationTypes = ref(false);
 
-    async function fetchPublicationTypes(params) {
+    async function fetchPublicationTypes(params: { lang: string }) {
       try {
         pendingPublicationTypes.value = true;
         const { data, error } = await useFetch("/api/publication_types", {
