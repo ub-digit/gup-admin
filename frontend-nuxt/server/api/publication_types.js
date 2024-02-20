788d267852e146dd36fe470d771ca9b0b0bb2f53
Johan Larsson <johan.larsson@ub.gu.se>
2024-02-20 13:48:16 +0100

    removes wrapper on data on server-side

--- frontend-nuxt/server/api/publication_types.js
+++ frontend-nuxt/server/api/publication_types.js
@@ -2,5 +2,5 @@ export default defineEventHandler(async (event) => {
   const config = useRuntimeConfig();
   const query = getQuery(event);
   const res = await $fetch(`${config.API_BASE_URL}/publication_types`, query);
-  return res;
+  return res.publication_types;
 });
