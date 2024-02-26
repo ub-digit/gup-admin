582d1d405b04620e8acd0c7a25363915fa5e7a81
Johan Larsson <johan.larsson@ub.gu.se>
2024-02-26 11:32:14 +0100

    adds types for the display_types coming from server

--- frontend-nuxt/types/PublicationCompareRow.ts
+++ frontend-nuxt/types/PublicationCompareRow.ts
@@ -2,9 +2,67 @@ interface PublicationCompareRow {
   diff: boolean;
   display_label: string;
   display_type: string;
-  first: any;
-  second?: any;
+  first:
+    | String
+    | Author
+    | Title
+    | Sourceissue_sourcepages_sourcevolume
+    | Url
+    | Meta;
+  second?:
+    | String
+    | Author
+    | Title
+    | Sourceissue_sourcepages_sourcevolume
+    | Url
+    | Meta;
   visibility: string;
+}
+
+interface String {
+  value: string;
+}
+
+interface Title {
+  value: {
+    title: string;
+    url: string;
+  };
+}
+
+interface Sourceissue_sourcepages_sourcevolume {
+  value: {
+    sourceissue: string;
+    sourcepages: string;
+    sourcevolume: string;
+  };
+}
+
+interface Author {
+  value: [
+    {
+      id?: number;
+      name: string;
+    },
+  ];
+}
+
+interface Url {
+  value: {
+    display_title: string;
+    url: string;
+  };
+}
+
+interface Meta {
+  value: {
+    attended: boolean;
+    created_at: string;
+    source: string;
+    updated_at: string;
+    version_created_by: string;
+    version_updated_by: string;
+  };
 }
 
 export type { PublicationCompareRow };
