09be75cdedb96d044632aa5da5e928e542fd842a
Lars Berglén <lars.berglen@ub.gu.se>
2024-02-29 12:51:25 +0100

    removed default api key from code

--- gup_index_manager/lib/gup_index_manager_web/controllers/controller_helpers.ex
+++ gup_index_manager/lib/gup_index_manager_web/controllers/controller_helpers.ex
@@ -1,8 +1,13 @@
 defmodule GupIndexManagerWeb.ControllerHelpers do
+
+  def check_api_key(nil) do
+    false
+  end
+
   def check_api_key(api_key) do
     api_key == get_api_key()
   end
   defp get_api_key() do
-    System.get_env("GUP_INDEX_MANAGER_API_KEY", "my_local_test_key")
+    System.get_env("GUP_INDEX_MANAGER_API_KEY")
   end
 end
