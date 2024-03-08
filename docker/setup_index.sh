8848fca9f721c875cf30010681452753c74e7e13
Lars Berglén <lars.berglen@ub.gu.se>
2024-03-08 11:26:00 +0100

    moved indexing file and made the index creation file simpler

--- docker/setup_index.sh
+++ docker/setup_index.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
-
-if [[ "$1" == "all" ]]; then
-    docker-compose exec index-manager-backend mix run -e "GupIndexManager.Maintenance.setup_indexes() |> IO.inspect()"
-elif [[ "$1" == "departments" ]]; then
-    docker-compose exec index-manager-backend mix run -e "GupIndexManager.Maintenance.setup_index("departments") |> IO.inspect()"
-elif [[ "$1" == "publications" ]]; then
-    docker-compose exec index-manager-backend mix run -e "GupIndexManager.Maintenance.setup_index("publications") |> IO.inspect()"
-elif [[ "$1" == "persons" ]]; then
-    docker-compose exec index-manager-backend mix run -e "GupIndexManager.Maintenance.setup_index("persons") |> IO.inspect()"
-fi
+docker-compose exec index-manager-backend mix run -e "GupIndexManager.Maintenance.setup_indexes() |> IO.inspect()"
+i
 
 
 
