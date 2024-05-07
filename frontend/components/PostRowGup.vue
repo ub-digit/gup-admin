3a2876a61645d488e92e8621a5d3b143d49ede16
Johan Larsson <johan.larsson@ub.gu.se>
2024-05-07 12:25:37 +0200

    add conditional for linking to GUP directly

--- frontend/components/PostRowGup.vue
+++ frontend/components/PostRowGup.vue
@@ -1,6 +1,38 @@
 <template>
   <div>
+    <a
+      v-if="linkToGup"
+      :href="`${config.public.API_GUP_BASE_URL}/publications/show/${post.id}`"
+      target="_blank"
+      class="list-group-item list-group-item-action"
+    >
+      <div class="d-flex w-100 justify-content-between">
+        <h5 class="title mb-1">{{ post_title_truncated }}</h5>
+      </div>
+      <p v-if="post.id" class="text-muted mb-2 d-none small">
+        ID: {{ post.id }}
+      </p>
+      <p class="mb-0">{{ post.pubyear }}</p>
+      <small>
+        {{ post.publication_type_label }}<br />
+        <ul
+          v-if="post.authors"
+          class="list-unstyled list-group list-group-horizontal mb-0"
+        >
+          <li v-for="(author, index) in post.authors" :key="author.id">
+            <span class="me-2" v-if="index < 3">
+              <span class="badge bg-secondary">{{ author.name }} </span>
+            </span>
+          </li>
+          <li v-if="post.authors.length > 3">
+            + {{ post.authors.length - 3 }}
+          </li>
+        </ul>
+      </small>
+    </a>
+
     <NuxtLink
+      v-else
       :to="{
         name: 'publications-post-id-gup-gupid-tab',
         query: $route.query,
@@ -36,8 +68,9 @@
 </template>
 
 <script lang="ts" setup>
-const props = defineProps(["post", "refresh"]);
+const props = defineProps(["post", "refresh", "linkToGup"]);
 const { t } = useI18n();
+const config = useRuntimeConfig();
 
 const maxLength = 85;
 
