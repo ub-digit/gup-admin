da8095e047e808388358409f7189778ac0dc0f8e
Johan Larsson <johan.larsson@ub.gu.se>
2024-05-07 12:59:29 +0200

    small layout changes

--- frontend/pages/authors/[id].vue
+++ frontend/pages/authors/[id].vue
@@ -5,31 +5,36 @@
         <h1>{{ authorPrimary?.first_name }} {{ authorPrimary?.last_name }}</h1>
       </div>
 
-      <div id="nameForms" class="row">
-        <strong>Namnform(er)</strong>
-
-        <ul class="list-unstyled">
-          <li
-            class="float-start"
-            v-for="(item, index) in author.names"
-            :key="item.gup_person_id"
-          >
-            <a
-              :href="`${config.public.API_GUP_BASE_URL}/admin/people/person/edit/${item.gup_person_id}`"
-              >{{ item.first_name }} {{ item.last_name }}</a
-            ><span v-if="item.primary" class="ms-1">*</span>
-            <span v-if="index < author.names.length - 1" class="me-1 ms-1"
-              >|</span
+      <div id="nameForms" class="row mb-3">
+        <div class="col-4">
+          <strong>Namnform(er)</strong>
+        </div>
+        <div class="col">
+          <ul class="list-unstyled">
+            <li
+              class="float-start"
+              v-for="(item, index) in author.names"
+              :key="index"
             >
-          </li>
-        </ul>
+              <a
+                :href="`${config.public.API_GUP_BASE_URL}/admin/people/person/edit/${item.gup_person_id}`"
+                >{{ item.first_name }} {{ item.last_name }}</a
+              ><span v-if="item.primary" class="ms-1">*</span>
+              <span v-if="index < author.names.length - 1" class="me-1 ms-1"
+                >|</span
+              >
+            </li>
+          </ul>
+        </div>
       </div>
 
       <div id="departments" class="row mb-3">
         <div class="col">
           <div class="row mb-3">
-            <div class="col">
+            <div class="col-4">
               <strong>Primär tillhörighet</strong>
+            </div>
+            <div class="col">
               <span v-if="authorCurrentDepartment">
                 {{ authorCurrentDepartment?.name }}
               </span>
@@ -37,8 +42,10 @@
             </div>
           </div>
           <div class="row">
-            <div class="col">
+            <div class="col-4">
               <strong>Alla tillhörigheter</strong>
+            </div>
+            <div class="col">
               <ul v-if="author?.departments?.length" class="list-unstyled">
                 <li
                   class="float-start"
@@ -62,9 +69,13 @@
       </div>
 
       <div id="yearOfBirth" class="row mb-3">
-        <div class="col">
+        <div class="col-4">
           <strong>Födelseår</strong>
-          <span v-if="author?.year_of_birth">{{ author.year_of_birth }}</span>
+        </div>
+        <div class="col">
+          <span v-if="author?.year_of_birth"
+            >&nbsp;{{ author.year_of_birth }}</span
+          >
           <span v-else> Saknas </span>
         </div>
       </div>
@@ -79,13 +90,13 @@
 
           <div class="row">
             <div class="col">
-              <table v-if="author?.identifiers?.length" class="table">
+              <table v-if="author?.identifiers?.length" class="table ms-2">
                 <tbody>
                   <tr v-for="identifier in author.identifiers">
                     <th scope="row">
                       {{
                         t(`views.authors.identifier_type.${identifier.code}`)
-                      }}:
+                      }}
                     </th>
                     <td>{{ identifier.value }}</td>
                   </tr>
