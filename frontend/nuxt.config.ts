77b74b81c84328982b6e5b9e1e87e48ce7e0c284
Johan Larsson <johan.larsson@ub.gu.se>
2024-05-13 14:46:35 +0200

    sets serverside renedering to true and removes trailing horunge

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,8 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false,
+  ssr: true,
   runtimeConfig: {
-    API_BASE_URL: "http://localhost:40411/",  // this should point to admin-backend
+    API_BASE_URL: "http://localhost:40411/", // this should point to admin-backend
     public: {
       API_GUP_BASE_URL: "http://localhost:3010", // this should point to gup-frontend and is used to redirect to posts for show/edit in gup-frontend
       ALLOW_AUTHOR_EDIT: true,
