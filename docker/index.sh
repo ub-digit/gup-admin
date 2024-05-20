cb742cecb393c97db5dd21ddc2b0ec914a11dfb2
Johan Larsson <johan.larsson@ub.gu.se>
2024-05-20 10:59:12 +0200

    removes indexing of departments

--- docker/index.sh
+++ docker/index.sh
@@ -1,6 +1,6 @@
 #docker-compose exec gup-backend bash -c 'bundle exec rake gup_admin:index_all_publications LIMIT=1000 OFFSET=0'
 docker-compose exec gup-imports bash -c 'python3 /data/scripts/put-scopus-docs.py -d /data/files/scopus-normalised/testdata -u $GUP_ADMIN_BASE_URL -a $GUP_ADMIN_API_KEY'
-docker-compose exec index-manager-backend mix run -e "GupIndexManager.Resource.Departments.initialize |> IO.inspect()" 
+#docker-compose exec index-manager-backend mix run -e "GupIndexManager.Resource.Departments.initialize |> IO.inspect()" 
 #docker-compose exec gup-backend bash -c 'bundle exec rake gup_admin:index_all_persons LIMIT=1000 OFFSET=0'
 # rm -rf venv 
 # python3 -m venv venv
