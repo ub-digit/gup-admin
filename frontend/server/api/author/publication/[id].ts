56900747146a6f04f7c6d7a7f3f25d26bd166d0e
Johan Larsson <johan.larsson@ub.gu.se>
2024-09-05 13:35:52 +0200

    removes stub-data

--- frontend/server/api/author/publication/[id].ts
+++ frontend/server/api/author/publication/[id].ts
@@ -6,33 +6,11 @@ export default defineEventHandler(async (event) => {
   const config = useRuntimeConfig();
   const query = getQuery(event);
   const id = event?.context?.params?.id;
-  console.log(id);
-  console.log(config.API_BASE_URL);
   const res: AuthorAffiliationArray = await $fetch(
     `${config.API_BASE_URL}/publications/${id}/authors`,
     {
       params: query,
     }
   );
-/*
-  const res: AuthorAffiliationArray = {
-    data: [
-      {
-        id: 1,
-        name: "John Doe 1",
-        affiliation_str: "University of Oslo",
-      },
-      {
-        id: 2,
-        name: "John Doe 2",
-        affiliation_str: "University of Oslo",
-      },
-      {
-        id: 3,
-        name: "John Doe 3",
-        affiliation_str: "University of Oslo",
-      },
-    ],
-  };*/
   return res;
 });
