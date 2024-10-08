5358d2fa29147e2cb029b701b23974fb68a45941
Johan Larsson <johan.larsson@ub.gu.se>
2024-10-08 10:30:16 +0200

    auth stuff added

---
+++ frontend/pages/login.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <button v-if="isLoggedIn" @click="handeSignOut">Sign Out</button>
+    <button v-else @click="handeSignIn">Sign in</button>
+
+    {{ data }}
+  </div>
+</template>
+
+<script setup lang="ts">
+const { status, data, signIn, signOut } = useAuth();
+
+const isLoggedIn = computed(() => status.value === "authenticated");
+
+async function handeSignIn() {
+  await signIn();
+}
+
+async function handeSignOut() {
+  await signOut();
+}
+</script>
---
+++ frontend/server/api/auth/[...].ts
@@ -0,0 +1,14 @@
+import { NuxtAuthHandler } from "#auth";
+
+import GithubProvider from "next-auth/providers/github";
+const config = useRuntimeConfig();
+export default NuxtAuthHandler({
+  config: useRuntimeConfig(),
+  providers: [
+    // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
+    GithubProvider.default({
+      clientId: config.GITHUB_CLIENT_ID,
+      clientSecret: config.GITHUB_CLIENT_SECRET,
+    }),
+  ],
+});
