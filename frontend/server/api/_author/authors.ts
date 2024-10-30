f7f58fb62e800671f6f3e74bd6c282f8c23c28e0
Johan Larsson <johan.larsson@ub.gu.se>
2024-10-30 11:38:49 +0100

    added underscore to folder to avoid triggering bug in nuxt-auth

--- frontend/server/api/_author/authors.ts
+++ frontend/server/api/_author/authors.ts
@@ -1,8 +1,5 @@
-import type {
-  AuthorAffiliation,
-  AuthorAffiliationArray,
-} from "~/types/Publication";
 import { getServerSession } from "#auth";
+import type { Author, AuthorResultList } from "~/types/Author";
 export default defineEventHandler(async (event) => {
   const session = await getServerSession(event);
   if (!session) {
@@ -10,12 +7,10 @@ export default defineEventHandler(async (event) => {
   }
   const config = useRuntimeConfig();
   const query = getQuery(event);
-  const id = event?.context?.params?.id;
-  const res: AuthorAffiliationArray = await $fetch(
-    `${config.API_BASE_URL}/publications/${id}/authors`,
-    {
-      params: query,
-    }
-  );
+  console.log(query);
+  console.log(config.API_BASE_URL);
+  const res: AuthorResultList = await $fetch(`${config.API_BASE_URL}/persons`, {
+    params: query,
+  });
   return res;
 });
--- frontend/server/api/_author/publication/[id].ts
+++ frontend/server/api/_author/publication/[id].ts
@@ -1,16 +1,19 @@
-import type { Author, AuthorResultList } from "~/types/Author";
+import type { AuthorAffiliationArray } from "~/types/Publication";
 import { getServerSession } from "#auth";
 export default defineEventHandler(async (event) => {
   const session = await getServerSession(event);
   if (!session) {
     return { status: 401, body: { message: "Unauthorized" } };
   }
+
   const config = useRuntimeConfig();
   const query = getQuery(event);
-  console.log(query);
-  console.log(config.API_BASE_URL);
-  const res: AuthorResultList = await $fetch(`${config.API_BASE_URL}/persons`, {
-    params: query,
-  });
+  const id = event?.context?.params?.id;
+  const res: AuthorAffiliationArray = await $fetch(
+    `${config.API_BASE_URL}/publications/${id}/authors`,
+    {
+      params: query,
+    }
+  );
   return res;
 });
