7b2b5dd6d48732e275aef7d96e6ac4e4c1ca194f
Lars Berglén <lars.berglen@ub.gu.se>
2024-11-13 22:56:53 +0100

    logs and http statuses

--- index_manager/lib/gup_index_manager_web/controllers/person_controller.ex
+++ index_manager/lib/gup_index_manager_web/controllers/person_controller.ex
@@ -7,10 +7,24 @@ defmodule GupIndexManagerWeb.PersonController do
 
     case ControllerHelpers.check_api_key(api_key) do
       true ->
-        json conn, Persons.Merger.merge(data) |> GupIndexManager.Resource.Persons.Execute.execute_actions()
+        Persons.Merger.merge(data) |> GupIndexManager.Resource.Persons.Execute.execute_actions() |> respond(conn)
       false ->
-        json conn, %{status: "error, unauthorized key"}
+        json Plug.Conn.put_status(conn, 401), %{status: "401", message: "error, unauthorized key"}
     end
+  end
+
+  def respond({:error, error, error_log_data}, conn) do
+    GupIndexManager.Resource.Logger.log({:error, error, error_log_data})
+    json Plug.Conn.put_status(conn, 500), %{status: "500", message: "Internal server error"}
+
+  end
+
+  def respond({:ok, _data, :no_actions}, conn) do
+    json Plug.Conn.put_status(conn, 200), %{status: "200", message: "No actions necessary"}
+  end
+
+  def respond({:ok, _res_data}, conn) do
+    json Plug.Conn.put_status(conn, 201), %{status: "201", message: "Person updated"}
   end
 
   def index(conn, _params) do
