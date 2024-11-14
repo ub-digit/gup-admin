ed0ede2ee91e17f3c2fba1507b5e02ca9801a4fb
Lars Berglén <lars.berglen@ub.gu.se>
2024-11-14 14:29:05 +0100

    fixed person model

--- index_manager/lib/gup_index_manager/model/person.ex
+++ index_manager/lib/gup_index_manager/model/person.ex
@@ -4,6 +4,8 @@ defmodule GupIndexManager.Model.Person do
 
   schema "persons" do
     field :json, :string
+    field :deleted, :boolean, default: false
+    field :deleted_at, :utc_datetime
     timestamps()
 
   end
@@ -11,7 +13,7 @@ defmodule GupIndexManager.Model.Person do
   @doc false
   def changeset(person, attrs) do
     person
-    |> cast(attrs, [:json])
+    |> cast(attrs, [:json, :deleted, :deleted_at])
     |> validate_required([:json])
   end
   def find_by_id(id) when is_nil(id), do: %GupIndexManager.Model.Person{}
