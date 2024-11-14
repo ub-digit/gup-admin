617d84389f09077718f047f8f8a32d7c4534f88e
Lars Berglén <lars.berglen@ub.gu.se>
2024-11-14 13:30:15 +0100

    Added env vars for  log file paths

--- index_manager/lib/gup_index_manager/resource/logger.ex
+++ index_manager/lib/gup_index_manager/resource/logger.ex
@@ -1,17 +1,18 @@
 defmodule GupIndexManager.Resource.Logger do
   @error_log_file "error.log"
-  @transaction_log_file "transaction.log"
+  @transactions_log_file "transaction.log"
   def log({:error, error, error_log_data}) do
-    IO.inspect("logging error")
+    file_path = System.get_env("ERROR_LOG_FILE_PATH") || @error_log_file
     now = DateTime.utc_now()
-    {:ok, file} = File.open(@error_log_file, [:write, :append])
+    {:ok, file} = File.open(file_path, [:write, :append])
     IO.binwrite(file, "[error] #{now} error:#{error}\n data:#{inspect(error_log_data)}\n")
     File.close(file)
   end
 
   def log({:transaction, actions, data}) do
+    file_path = System.get_env("TRANSACTIONS_LOG_FILE_PATH") || @transactions_log_file
     now = DateTime.utc_now()
-    {:ok, file} = File.open(@transaction_log_file, [:write, :append])
+    {:ok, file} = File.open(file_path, [:write, :append])
     IO.binwrite(file, "[info] #{now} actions: #{inspect(actions)}\n data: #{inspect(data)}\n")
     File.close(file)
   end
