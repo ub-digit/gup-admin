3c4f98c500add2b3beed7415b2d68ca4c7cb370a
Lars Berglén <lars.berglen@ub.gu.se>
2024-12-02 21:58:19 +0100

    passing is_merged=true to /persons route will return users witt more than one name form

--- backend/lib/gup_admin_web/controllers/person_controller.ex
+++ backend/lib/gup_admin_web/controllers/person_controller.ex
@@ -3,10 +3,21 @@ defmodule GupAdminWeb.PersonController do
   alias GupAdmin.Resource.Person
 
   def search(conn, %{"query" => q}) do
+    IO.inspect("WHAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAT")
     json conn, Person.search_persons(q)
   end
 
+  def search(conn, %{"is_merged" => "true"}) do
+    IO.inspect("KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKk")
+    # json conn, Person.get_all_persons() |> Enum.filter(fn person -> length(Map.get(person, "names", [])) > 1 end)
+    d = Person.get_all_persons() |> Map.get("data") |> Enum.filter(fn person -> length(Map.get(person, "names", [])) > 1 end)
+    json conn, d
+
+
+  end
+
   def search(conn, _params) do
+    IO.inspect("ssssssssssssssssssssssssssssssssssssssssssssssssss  ")
     json conn, Person.get_all_persons()
   end
 
