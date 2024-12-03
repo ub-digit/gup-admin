ddf3da6a25adac203d02e27f02cde22a0c5fadff
Lars Berglén <lars.berglen@ub.gu.se>
2024-12-03 09:45:25 +0100

    correct data format in person controller response

--- backend/lib/gup_admin_web/controllers/person_controller.ex
+++ backend/lib/gup_admin_web/controllers/person_controller.ex
@@ -6,7 +6,9 @@ defmodule GupAdminWeb.PersonController do
     IO.inspect("KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKk")
     # json conn, Person.get_all_persons() |> Enum.filter(fn person -> length(Map.get(person, "names", [])) > 1 end)
     d = Person.get_all_persons() |> Map.get("data") |> Enum.filter(fn person -> length(Map.get(person, "names", [])) > 1 end)
-    json conn, d
+    r = %{"data" => d, "showing" => length(d), "total" => length(d)}
+    IO.inspect(r)
+    json conn, %{"data" => d, "showing" => length(d), "total" => length(d)}
 
 
   end
