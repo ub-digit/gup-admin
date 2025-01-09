abe6bb358bea1d79905bc99f06ce51e08322d7b1
Johan Larsson <johan.larsson@ub.gu.se>
2025-01-09 14:14:05 +0100

    first hack for person-edit

---
+++ frontend/components/ActionsAuthor.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="row">
+      <div class="col d-flex justify-content-end">
+        <NuxtLink
+          class="btn btn-link"
+          :to="{
+            name: 'authors-id-edit',
+            query: $route.query,
+            params: { id: author.id },
+          }"
+        >
+          Redigera
+        </NuxtLink>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { storeToRefs } from "pinia";
+import type { Nameform, Department, Author } from "~/types/Author";
+import { useAuthorsStore } from "~/store/authors";
+const storeAuthor = useAuthorsStore();
+const { author } = storeToRefs(storeAuthor);
+</script>
+
+<style scoped></style>
---
+++ frontend/server/api/_author/[id].put.ts
@@ -0,0 +1,23 @@
+import type { Author } from "~/types/Author";
+import { getServerSession } from "#auth";
+export default defineEventHandler(async (event) => {
+  const session = await getServerSession(event);
+  if (!session) {
+    return { status: 401, body: { message: "Unauthorized" } };
+  }
+  const config = useRuntimeConfig();
+  const query = getQuery(event);
+  const id = event?.context?.params?.id;
+  const data = await readBody(event);
+  return data;
+
+  /*
+  console.log("from put to author");
+  console.log("id", id);
+  console.log("data", data);
+  const res = await $fetch(`${config.API_BASE_URL}/person/${id}`, {
+    method: "POST",
+    body: JSON.stringify(data),
+  });
+  return res;*/
+});
