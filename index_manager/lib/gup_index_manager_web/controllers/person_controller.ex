3b5fc3c6065c626aff7f5e25a52b89c8d5ddd05e
Lars Berglén <lars.berglen@ub.gu.se>
2025-01-30 09:27:25 +0100

    check for x_account_status in incomming data and reject if its anything else but 'active'

--- index_manager/lib/gup_index_manager_web/controllers/person_controller.ex
+++ index_manager/lib/gup_index_manager_web/controllers/person_controller.ex
@@ -3,8 +3,35 @@ defmodule GupIndexManagerWeb.PersonController do
   alias GupIndexManager.Resource.Persons
   alias GupIndexManagerWeb.ControllerHelpers
 
-  def create_or_update(conn,  %{"data" => data, "api_key" => api_key}) do
 
+  @doc """
+  Handles the creation or update of a person based on the provided data.
+
+  ## Parameters
+  - conn: The connection struct.
+  - params: A map containing the following keys:
+    - "data": A map with the key "x_account_status" which should be "inactive".
+    - "api_key": The API key for authorization.
+
+  ## Responses
+  - Returns a JSON response with status 200 and a message indicating that no actions are necessary if the account is inactive and the API key is valid.
+  - Returns a JSON response with status 401 and an error message if the API key is invalid.
+  """
+  def create_or_update(conn, %{"data" => %{"x_account_status" => account_status} = data, "api_key" => api_key}) do
+    case ControllerHelpers.check_api_key(api_key) do
+      true ->
+        case account_status do
+          "active" ->
+            Persons.Merger.merge(data) |> GupIndexManager.Resource.Persons.Execute.execute_actions() |> respond(conn)
+          _ ->
+            json Plug.Conn.put_status(conn, 200), %{status: "200", message: "x_account_status property present but not set to active, no actions necessary"}
+        end
+      false ->
+        json Plug.Conn.put_status(conn, 401), %{status: "401", message: "error, unauthorized key"}
+    end
+  end
+
+  def create_or_update(conn,  %{"data" => data, "api_key" => api_key}) do
     case ControllerHelpers.check_api_key(api_key) do
       true ->
         Persons.Merger.merge(data) |> GupIndexManager.Resource.Persons.Execute.execute_actions() |> respond(conn)
