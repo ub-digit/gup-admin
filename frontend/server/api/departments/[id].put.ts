360e45feeffdd0c2cd81bb0e6c35731f395dff81
Johan Larsson <johan.larsson@ub.gu.se>
2025-04-08 11:08:35 +0200

    add server route for put for departments

---
+++ frontend/server/api/departments/[id].put.ts
@@ -0,0 +1,19 @@
+import { getServerSession } from "#auth";
+export default defineEventHandler(async (event) => {
+  const session = await getServerSession(event);
+  if (!session) {
+    return { status: 401, body: { message: "Unauthorized" } };
+  }
+  const config = useRuntimeConfig();
+  const query = getQuery(event);
+  query.api_key = config.ADMIN_BACKEND_API_KEY;
+  const id = event?.context?.params?.id;
+  const data = await readBody(event);
+
+  const res = await $fetch(`${config.API_BASE_URL}/api/departments/${id}`, {
+    method: "PUT",
+    body: JSON.stringify({ data }),
+    params: query,
+  });
+  return res;
+});
