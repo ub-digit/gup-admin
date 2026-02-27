3b422b8a8dd9366940b675467602654d80b3acb3
David  Gustafsson <david.gustafsson@ub.gu.se>
2026-02-27 15:57:03 +0100

    Use ansible for setting up local docker environment

--- ansible/run-playbook.sh
+++ ansible/run-playbook.sh
@@ -6,7 +6,7 @@ if [[ -z "$2" ]]; then
     exit 0;
 fi
 
-targets="dev staging lab production"
+targets="staging lab production dev"
 target=$1
 playbook=$2
 
@@ -25,4 +25,4 @@ shift;
 shift;
 
 set -x
-ansible-playbook --vault-password-file .vault_password -i inventory/$target.yml  $playbook.yml $@
+ansible-playbook --vault-password-file ~/.config/ub-ansible-deploy/.vault_password -i inventory/$target.yml  $playbook.yml $@
